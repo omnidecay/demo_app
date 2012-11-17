718def131b9256bfc76f9f0641f687e07d79bf42
omnidecay <omnidecay@omnidecay.(none)>
2012-11-17 10:25:57 -0600

    initialCommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '7e6caff4befc0ad754531170d6b9220781378cf571f1a8574073ab135e04953c27e596014e7bd8f844b47f4ac1ed80227d0edb6e426fdcd5ac5aaedb97883810'
